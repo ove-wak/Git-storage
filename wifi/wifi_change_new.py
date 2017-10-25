c8b0340806e1f0c744754d1c4f692a297be8e9c3
ove-wak <ove.wak@gmail.com>
2017-10-25 15:05:03 +0800

    wifi:change

--- wifi/wifi_change_new.py
+++ wifi/wifi_change_new.py
@@ -4,7 +4,7 @@ time = []#时间戳列表
 wifi_content = []#wifi列表
 time_diff = []#时间差列表
 time_diff_add = []#时间差位置列表
-filename = "1"
+filename = "2"
 with open('B'+filename+'tampn.csv') as f: 
     f_csv = csv.reader(f)
     headers = next(f_csv)
@@ -32,6 +32,13 @@ for x in range(len_time):
 while len(time_diff_add) < len_time_diff:
     time_diff_add.append(-1)
 
+for x in range(len(wifi_content)):
+    for y in range(len(wifi_content[0])):
+        if wifi_content[x][y] == "":
+            wifi_content[x][y] = -95
+        else:
+            wifi_content[x][y] = int(wifi_content[x][y])
+
 wifi_change = []
 for x in range(0,len(time_diff_add),2):
     if time_diff_add[x+1] == -1 or time_diff_add[x+1] == 0:
@@ -48,25 +55,35 @@ for y in range(0,len(wifi_change),2):
     temp = 0
     if wifi_change[y] == -1:
         wifi_change_new.append(-1)
-        wifi_change_new.append(-1)
     else:
         wifi_change_new.append([])
-        wifi_change_new.append([]) 
         for x in range(len(wifi_change[y])):
-            if wifi_change[y][x] != wifi_change[y+1][x]:
-                wifi_change_new[y].append(wifi_change[y][x])
-                wifi_change_new[y+1].append(wifi_change[y+1][x])
-print(wifi_change_new) ##结果为-1的值表示无法判断 
-print(len(wifi_change_new))   
+            if abs(wifi_change[y][x] - wifi_change[y+1][x]) > 5:
+                if wifi_change[y][x] > -80 or wifi_change[y+1][x] > -80:
+                    wifi_change_new[int(y/2)].append([wifi_change[y][x],wifi_change[y+1][x]])
+  
+wifi_change_new_sort = []
+for x in wifi_change_new:
+    if x == -1:
+        wifi_change_new_sort.append(-1)
+        wifi_change_new_sort.append(-1) 
+    else:
+        wifi_change_new_sort.append([])
+        wifi_change_new_sort.append([])
+        x.sort(reverse=True)
+        for y in x:
+            wifi_change_new_sort[len(wifi_change_new_sort)-2].append(y[0])
+            wifi_change_new_sort[len(wifi_change_new_sort)-1].append(y[1])
+print(wifi_change_new_sort)
 
 with open('wifi_'+filename+'_new_content.csv','w', newline='') as f:##如果不加newline,存的文件里每隔一行会多一个空行
     f_csv = csv.writer(f)
     f_csv.writerow([['time'],['wifi_change']])#如果直接传入字符串的话,该方法会把每个字符作为单个列表元素来处理
     for x in range(len(time_diff)):
-        if wifi_change_new[x] == -1:
-            f_csv.writerow([str(time_diff[x])] + [str(wifi_change_new[x])])
+        if wifi_change_new_sort[x] == -1:
+            f_csv.writerow([str(time_diff[x])] + [str(wifi_change_new_sort[x])])
         else:
-            f_csv.writerow([str(time_diff[x])] + wifi_change_new[x])
+            f_csv.writerow([str(time_diff[x])] + wifi_change_new_sort[x])
         if x%2 == 1:
             f_csv.writerow('\n')
-    f_csv.writerow(['注释:-1表示无法判断;每两行为一组数据,同一组内去除了相同的wifi(即同时存在但数值相同或都不存在)'])
+    f_csv.writerow(['注释:-1表示无法判断;-95的值是对空值的标准化;每两行为一组数据,同一组内去除了相差不大于5的值,去除了信号一直微弱的值,并对剩下的值排序'])
