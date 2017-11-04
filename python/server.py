3c8a82847c7a4927b556820645e56d97d564d86d
wak <ove.wak@gmail.com>
2017-11-04 16:16:29 +0800

    python httpserver

---
+++ python/server.py
@@ -0,0 +1,25 @@
+from http.server import HTTPServer,BaseHTTPRequestHandler     
+import io,shutil,urllib,json
+
+class MyHttpHandler(BaseHTTPRequestHandler):
+    def do_POST(self):
+        length = int(self.headers['Content-Length'])
+        readdata = self.rfile.read(length).decode('utf-8')
+        post_data = json.loads(readdata)
+        # You now have a dictionary of the post data
+        print(post_data)
+        data = json.dumps({'name':'wak'})
+        enc="UTF-8"  
+        encoded = ''.join(data).encode(enc)  
+        f = io.BytesIO()  
+        f.write(encoded)  
+        f.seek(0)  
+        self.send_response(200)  
+        self.send_header("Content-type", "text/html; charset=%s" % enc)  
+        self.send_header("Content-Length", str(len(encoded)))  
+        self.end_headers()  
+        shutil.copyfileobj(f,self.wfile)
+    
+httpd=HTTPServer(('',9601),MyHttpHandler)     
+print("Server started on 127.0.0.1,port 9601......")     
+httpd.serve_forever() 
