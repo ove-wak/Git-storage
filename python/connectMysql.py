8012eff6d5c3db848c6cc78269f5e49ed514b780
ove-wak <ove.wak@gmail.com>
2017-11-27 17:31:01 +0800

    commit all

--- python/connectMysql.py
+++ python/connectMysql.py
@@ -1,129 +1,139 @@
 import pymysql
 
-# 连接数据库
-def connect(self):
-    db = pymysql.connect(host="localhost",
-                         port=3306,
-                         user='root',
-                         password='123456',
-                         db='test_database')
-    return db
+class ConnectMysql:
+    # 初始化类连接数据库
+    def __init__(self):
+        self.db = pymysql.connect(host="localhost",
+                             port=3306,
+                             user='root',
+                             password='123456',
+                             db='test_database')
 
-# 关闭数据库连接
-def close_conn(self,db):
-    db.close()
+    # 关闭连接
+    def close_conn(self):
+        self.db.close()
 
-# 创建表
-def create_table(self,db):
-    cursor = db.cursor()
-    # 创建指纹记录表
-    sql = """CREATE TABLE IF NOT EXISTS fingerprint_record(
-             id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-             phone_ip CHAR(15) NOT NULL,
-             signal_type INT NOT NULL,
-             coordinate_x INT NOT NULL,
-             coordinate_y INT NOT NULL,
-             signal_time VARCHAR(40))
-             """
-    cursor.execute(sql)
-    # 创建信号记录表
-    sql = """CREATE TABLE IF NOT EXISTS signal_record(
-             id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-             record_id INT UNSIGNED NOT NULL,
-             signal_mac_address VARCHAR(20),
-             signal_strength INT NOT NULL)
-             """
-    cursor.execute(sql)
-    cursor.close()
-
-# 删除表
-def drop_table(self,db):
-    cursor = db.cursor()
-    cursor.execute("DROP TABLE IF EXISTS fingerprint_record")
-    cursor.execute("DROP TABLE IF EXISTS signal_record")
-    cursor.close()
-
-# 插入数据
-def insert_data(self,db):
-    cursor = db.cursor()
-    #
-    #
-    #
-    sql = ""
-    flag = 0 # 是否执行成功标记
-    try:
-        # 执行sql语句
+    # 创建表
+    # 对于没有 phone_ip 的数据,人为标识区分
+    def create_table(self):
+        cursor = self.db.cursor()
+        # 创建指纹记录表
+        sql = "CREATE TABLE IF NOT EXISTS fingerprint_record(\
+                 id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,\
+                 address VARCHAR(20) NOT NULL,\
+                 phone_ip CHAR(15) NOT NULL,\
+                 signal_type INT NOT NULL,\
+                 coordinate_x INT NOT NULL,\
+                 coordinate_y INT NOT NULL,\
+                 signal_time VARCHAR(40));"
         cursor.execute(sql)
-        # 提交到数据库执行
-        db.commit()
-        flag = 1
-    except:
-        # 如果发生错误则回滚
-        db.rollback()
-        flag = -1
-    cursor.close()
-    return flag
-
-# 查询数据
-def select_data(self,db):
-    cursor = db.cursor()
-    #
-    #
-    #
-    sql = ""
-    flag = 0 # 是否执行成功标记
-    try:
-        # 执行sql语句
+        # 创建信号记录表
+        sql = "CREATE TABLE IF NOT EXISTS signal_record(\
+                 id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,\
+                 record_id INT UNSIGNED NOT NULL,\
+                 signal_mac_address VARCHAR(20),\
+                 signal_strength INT NOT NULL);"
         cursor.execute(sql)
-        # 提交到数据库执行
-        db.commit()
-        flag = 1
-    except:
-        # 如果发生错误则回滚
-        db.rollback()
-        flag = -1
-    cursor.close()
-    return flag
+        cursor.close()
 
-# 更新数据
-def update_data(self,db):
-    cursor = db.cursor()
-    #
-    #
-    #
-    sql = ""
-    flag = 0 # 是否执行成功标记
-    try:
-        # 执行sql语句
-        cursor.execute(sql)
-        # 提交到数据库执行
-        db.commit()
-        flag = 1
-    except:
-        # 如果发生错误则回滚
-        db.rollback()
-        flag = -1
-    cursor.close()
-    return flag
+    # 删除表
+    def drop_table(self):
+        cursor = self.db.cursor()
+        cursor.execute("DROP TABLE IF EXISTS fingerprint_record")
+        cursor.execute("DROP TABLE IF EXISTS signal_record")
+        cursor.close()
 
-# 删除数据
-def delete_data(self,db):
-    cursor = db.cursor()
-    #
-    #
-    #
-    sql = ""
-    flag = 0 # 是否执行成功标记
-    try:
-        # 执行sql语句
-        cursor.execute(sql)
-        # 提交到数据库执行
-        db.commit()
-        flag = 1
-    except:
-        # 如果发生错误则回滚
-        db.rollback()
-        flag = -1
-    cursor.close()
-    return flag
+    # 插入数据
+    def insert_data(self,addr,phoneIP,strtype,x,y,time,mac,ap):
+        cursor = self.db.cursor()
+        sql = "INSERT INTO fingerprint_record VALUES(NULL, '" + addr + "', '" + phoneIP + "', " + str(strtype) + ", " + str(x) + ", " + str(y) + ", '" + time + "');"
+        flag = 0 # 是否执行成功标记
+        try:
+            # 执行sql语句
+            cursor.execute(sql)
+            # 提交到数据库执行
+            self.db.commit()
+            flag = 1
+            strRecordID = str(cursor.lastrowid)
+            for i in range(len(mac)):
+                sql ="INSERT INTO signal_record VALUES(NULL, " + strRecordID + ", '" + mac[i] + "', " + str(ap[i]) + ");"  
+                try:
+                    cursor.execute(sql)
+                    self.db.commit()
+                    flag = 1  
+                except:
+                    self.db.rollback()
+                    flag = -1
+                    break
+        except:
+            # 如果发生错误则回滚
+            self.db.rollback()
+            flag = -1
+        cursor.close()
+        return flag
+
+    # 查询数据**待完成
+    def select_data(self):
+        cursor = self.db.cursor()
+        #
+        #
+        #
+        sql = ""
+        flag = 0 # 是否执行成功标记
+        try:
+            # 执行sql语句
+            cursor.execute(sql)
+            # 提交到数据库执行
+            self.db.commit()
+            flag = 1
+        except:
+            # 如果发生错误则回滚
+            self.db.rollback()
+            flag = -1
+        cursor.close()
+        return flag
+
+    # 更新数据
+    # *暂不需要
+    def update_data(self):
+        cursor = self.db.cursor()
+        #
+        #
+        #
+        sql = ""
+        flag = 0 # 是否执行成功标记
+        try:
+            # 执行sql语句
+            cursor.execute(sql)
+            # 提交到数据库执行
+            self.db.commit()
+            flag = 1
+        except:
+            # 如果发生错误则回滚
+            self.db.rollback()
+            flag = -1
+        cursor.close()
+        return flag
+
+    # 删除数据
+    # *暂不需要
+    def delete_data(self):
+        cursor = self.db.cursor()
+        #
+        #
+        #
+        sql = ""
+        flag = 0 # 是否执行成功标记
+        try:
+            # 执行sql语句
+            cursor.execute(sql)
+            # 提交到数据库执行
+            self.db.commit()
+            flag = 1
+        except:
+            # 如果发生错误则回滚
+            self.db.rollback()
+            flag = -1
+        cursor.close()
+        return flag
 
---
+++ python/saveData.py
@@ -0,0 +1,55 @@
+import os
+from connectMysql import ConnectMysql
+path = "C:/Users/ove_wak/Desktop/git-storage/OS-ELM-matlab/wifiinfo/"
+path_time = "11.17/"
+file_name = []
+
+# 连接数据库
+conn = ConnectMysql()
+
+# 如果表不存在则创建表
+conn.create_table()
+
+# 获取指定目录下所有数据的文件名
+tt = os.walk(path + path_time)
+for i in tt:
+    for j in i[2]:
+        if ".txt" in j:
+            file_name.append(j)
+
+# 遍历指定目录下所有数据文件并插入到数据库中
+for file in file_name:
+    with open(path + path_time + file, 'r') as file_read:
+        file_a = file.split('.')
+        file_b = file_a[0].split('-')
+        addr = file_b[0]
+        strx = file_b[1] # 注意x,y的先后
+        stry = file_b[2]
+        phoneIP = strx + ', ' +stry # ##phoneIP为采集中缺失的参数,暂时用位置和代替
+        while True:
+            lines = file_read.readline() # 读取整行数据
+            if not lines:
+                break
+            line_data = lines.split()
+            line_time = line_data.pop(0)
+            line_time = line_time + " " + line_data.pop(0)
+            mac = []
+            ap = []
+            if line_data:
+                for x in range(0,2,len(line_data)):
+                    mac.append(line_data[x])
+                    ap.append(line_data[x+1])
+
+            flag = conn.insert_data(addr,phoneIP,1,strx,stry,line_time,mac,ap)  
+            if flag == -1:
+                print("数据库插入数据出错")
+                break       
+
+
+# 数据库中的数据需要全部清掉,代码好像还有点逻辑上的问题,主键跟外键一直是一样的.
+# 跑的太慢,加个时间的代码
+
+
+
+
+conn.close_conn()
