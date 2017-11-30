2f0ce95282b93579dede41ebb9964bc07ac031be
ove-wak <ove.wak@gmail.com>
2017-11-30 16:45:55 +0800

    pychange

--- python/gui.py
+++ python/gui.py
@@ -1,19 +1,31 @@
 from tkinter import *
 
-class Application(Frame):
-    def __init__(self, master=None):
-        Frame.__init__(self, master)
-        self.pack()
-        self.createWidgets()
+class Gui_content:
+    def __init__(self, master):
+        #使用Frame增加一层容器
+        fm1 = Frame(master)
+        self.l1 = Label(fm1,bg="gray", text="ewe")
+        self.l1.pack(fill=BOTH,expand=YES,side=TOP,anchor=W,ipadx=3,ipady=3,pady=10,padx=30)
+        fm1.pack(side=LEFT, fill=BOTH, expand=YES)
 
-    def createWidgets(self):
-        self.helloLabel = Label(self, text='Hello, world!')
-        self.helloLabel.pack()
-        self.quitButton = Button(self, text='Quit', command=self.quit)
-        self.quitButton.pack()
+        fm2 = Frame(master)
+        b1 = Button(fm2,text="完整数据存储")
+        b1.bind("<Button-1>",self.button_event)
+        b1['width'] = 15
+        b1['height'] = 1
+        b1.pack(ipady=3,pady=10,padx=30)
+        b2 = Button(fm2,text="底图数据存储")
+        b2['width'] = 15
+        b2['height'] = 1
+        b2.pack(ipady=3,pady=10,padx=30)     
+        fm2.pack(side=LEFT, padx=10)
 
-app = Application()
-# 设置窗口标题:
-app.master.title('指纹更新')
-# 主消息循环:
-app.mainloop()
+    def button_event(self, event):
+        self.l1['text'] = "button on"
+
+root = Tk()
+root.title("指纹更新")
+root.geometry("800x600")
+root.resizable(width=False, height=False)
+display = Gui_content(root)
+root.mainloop()
