c5ba7e5de6eedf38d33e1a636ceff94e8725e983
ove-wak <ove.wak@gmail.com>
2017-12-19 21:04:46 +0800

    数据处理

--- python/指纹更新/connectMysql.py
+++ python/指纹更新/connectMysql.py
@@ -1,4 +1,5 @@
 import pymysql
+import xlwt
 
 class ConnectMysql:
     # 初始化类连接数据库
@@ -7,7 +8,7 @@ class ConnectMysql:
                              port=3306,
                              user='root',
                              password='123456',
-                             db='test_1129')
+                             db='wifi_db')
 
     # 关闭连接
     def close_conn(self):
@@ -20,11 +21,13 @@ class ConnectMysql:
         # 创建指纹记录表
         sql = "CREATE TABLE IF NOT EXISTS fingerprint_record(\
                  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,\
+                 model_num INT NOT NULL,\
                  address VARCHAR(20) NOT NULL,\
-                 phone_ip CHAR(15) NOT NULL,\
+                 phone_ip VARCHAR(20) NOT NULL,\
                  signal_type INT NOT NULL,\
                  coordinate_x INT NOT NULL,\
                  coordinate_y INT NOT NULL,\
+                 direction VARCHAR(6),\
                  signal_time VARCHAR(40));"
         cursor.execute(sql)
         # 创建信号记录表
@@ -44,9 +47,9 @@ class ConnectMysql:
         cursor.close()
 
     # 插入数据
-    def insert_data(self,addr,phoneIP,strtype,x,y,time,mac,ap):
+    def insert_data(self,model,addr,phoneIP,strtype,x,y,direction,time,mac,ap):
         cursor = self.db.cursor()
-        sql = "INSERT INTO fingerprint_record VALUES(NULL, '" + addr + "', '" + phoneIP + "', " + str(strtype) + ", " + str(x) + ", " + str(y) + ", '" + time + "');"
+        sql = "INSERT INTO fingerprint_record VALUES(NULL, '" + str(model) + "', '" + addr + "', '" + phoneIP + "', " + str(strtype) + ", " + str(x) + ", " + str(y) + ", '" + direction + "', '" + time + "');"
         flag = 0 # 是否执行成功标记
         try:
             # 执行sql语句
@@ -74,26 +77,47 @@ class ConnectMysql:
         cursor.close()
         return flag
 
-    # 查询数据**待完成
+    # 预处理得到底图
     def select_data(self):
+        num = 0
+        ap_mac = ('d8:15:0d:6c:13:98','00:90:4c:5f:00:2a','ec:17:2f:94:82:fc','70:ba:ef:d5:a6:12')
+        ditu = [[0,0,0,0,0,0,0,0,0,0,0,0,0],
+                [0,0,0,0,0,0,0,0,0,0,0,0,0],
+                [0,0,0,0,0,0,0,0,0,0,0,0,0],
+                [0,0,0,0,0,0,0,0,0,0,0,0,0],
+                [0,0,0,0,0,0,0,0,0,0,0,0,0],
+                [0,0,0,0,0,0,0,0,0,0,0,0,0],
+                [0,0,0,0,0,0,0,0,0,0,0,0,0],
+                [0,0,0,0,0,0,0,0,0,0,0,0,0],
+                [0,0,0,0,0,0,0,0,0,0,0,0,0],
+                [0,0,0,0,0,0,0,0,0,0,0,0,0]]
+
         cursor = self.db.cursor()
-        #
-        #
-        #
-        sql = ""
-        flag = 0 # 是否执行成功标记
-        try:
-            # 执行sql语句
-            cursor.execute(sql)
-            # 提交到数据库执行
-            self.db.commit()
-            flag = 1
-        except:
-            # 如果发生错误则回滚
-            self.db.rollback()
-            flag = -1
+        sql = "select id,coordinate_x,coordinate_y from fingerprint_record where model_num=0;"
+        cursor.execute(sql)    
+        results=cursor.fetchall()
+        results = list(results)
+        for result in results:
+            if ditu[result[1]][result[2]] == 0:
+                ditu[result[1]][result[2]] = []
+            ditu[result[1]][result[2]].append(result[0])
+        for x in range(len(ditu)):
+            for y in range(len(ditu[0])):
+                if ditu[x][y] != 0:
+                    temp = ditu[x][y]
+                    ditu[x][y] = []
+                    for mac in ap_mac:
+                        sql = "select signal_strength from signal_record where record_id IN "+str(tuple(temp))+" and signal_mac_address='"+mac+"';" 
+                        cursor.execute(sql)
+                        res=cursor.fetchall()
+                        num = 0
+                        for r in res:
+                            num = num + r[0]
+                        ditu[x][y].append(int(num/len(res)))
+                    print(ditu[x][y]) 
+        print(ditu)# 处理得到底图原始数据,在 底图.py 文件里进行插值并保存为excel得到完整底图
         cursor.close()
-        return flag
+        return 1
 
     # 更新数据
     # *暂不需要
--- python/指纹更新/saveData.py
+++ python/指纹更新/saveData.py
@@ -8,49 +8,26 @@ class SaveData:
         self.conn.create_table()
 
     # 完整数据保存               
-    def complete_data_save(self,path,file):    
+    def data_save(self,path,file,phone_model):    
         with open(path + file, 'r') as file_read:
             file_a = file.split('.')
             file_b = file_a[0].split('-')
-            addr = file_b[0]
-            strx = file_b[1] # 注意x,y的先后
-            stry = file_b[2]
-            phoneIP = strx + ', ' +stry # ##phoneIP为采集中缺失的参数,暂时用位置和代替
-            while True:
-                lines = file_read.readline() # 读取整行数据
-                if not lines:
-                    break
-                line_data = lines.split()
-                line_time = line_data.pop(0)
-                line_time = line_time + " " + line_data.pop(0)
-                mac = []
-                ap = []
-                if line_data:
-                    for x in range(0,len(line_data),2):
-                        mac.append(line_data[x])
-                        ap.append(line_data[x+1])
-                flag = self.conn.insert_data(addr,phoneIP,1,strx,stry,line_time,mac,ap)  
-                if flag == -1:
-                    return line_time        
-        return 1
-
-    #底图数据,只保存中间三分之一
-    def initial_data_save(self,path,file):
-        with open(path + file, 'r') as file_read:
-            file_a = file.split('.')
-            file_b = file_a[0].split('-')
-            addr = file_b[0]
-            strx = file_b[1] # 注意x,y的先后
-            stry = file_b[2]
-            phoneIP = strx + ', ' +stry # ##phoneIP为采集中缺失的参数,暂时用位置和代替
+            addr = "C204"
+            phoneIP = phone_model
+            model = file_b[0]
+            coordinate = file_b[1] # 注意x,y的先后
+            strx = coordinate[len(coordinate) - 1]
+            stry = coordinate[0:(len(coordinate) - 1)]
+            direction = file_b[2]
             line_datas = []
             while True:
                 lines = file_read.readline() # 读取整行数据
                 if not lines:
                     break
                 line_datas.append(lines.split())
-            one = int(len(line_datas)/3)
-            line_datas = line_datas[one:one*2]
+            if int(model) == 0: # 模型0为底图采集,只保存中间三分之一数据 
+                one = int(len(line_datas)/3)
+                line_datas = line_datas[one:one*2]
             for line_data in line_datas:
                 line_time = line_data.pop(0)
                 line_time = line_time + " " + line_data.pop(0)
@@ -60,10 +37,16 @@ class SaveData:
                     for x in range(0,len(line_data),2):
                         mac.append(line_data[x])
                         ap.append(line_data[x+1])
-                flag = self.conn.insert_data(addr,phoneIP,1,strx,stry,line_time,mac,ap)  
+                flag = self.conn.insert_data(model,addr,phoneIP,1,strx,stry,direction,line_time,mac,ap)  
                 if flag == -1:
-                    return line_time     
+                    return line_time        
         return 1
 
+    # 读取数据
+    def data_read(self):
+        data = self.conn.select_data()
+        return data
+
+    #关闭连接
     def close_connect(self):
         self.conn.close_conn()
