31de5d2307e7038f3a9355266ac3268a2b0299a6
ove_wak <ove.wak@gmail.com>
2018-01-09 20:18:07 +0800

    flask

--- python/flask/flaskTest.py
+++ python/flask/flaskTest.py
@@ -1,28 +1,40 @@
-from flask import Flask
+from flask import Flask,url_for,redirect
 from flask import render_template
+from flask import request
 
 app = Flask(__name__)#没太懂?
 
 @app.route('/')#为下一行函数绑定url路径
-def hello_world():
-    return '<h1>Hello World!</h1>'
+def index():
+    login_url = url_for('login')
+    return redirect(login_url) # 重定向
 
 @app.route('/<username>')#url变量规则
 def hello_test(username):
     return 'Hello %s' % username
 
-@app.route('/a/')
-def aaa():
-    return '<h1>aaa</h1>'
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if valid_login(request.form['username'],
+                       request.form['password']):
+            return redirect(url_for('hello',name=request.form['username']))
+        else:
+            error = 'Invalid username/password'
+    return render_template('login.html', error=error)
 
-@app.route('/a')
-def a():
-    return '<h1>a</h1>'
+def valid_login(name,password):
+    if name == 'wak' and password == '123456':
+        return 1
+    else:
+        return 0
 
-@app.route('/hello/')
-@app.route('/hello/<name>')
-def hello(name=None):
+@app.route('/hello')
+def hello():
+    name = request.args.get('name')
     return render_template('hello.html', name=name)
+       
 
 if __name__ == '__main__':
     # run 启动服务器
---
+++ python/flask/static/readme.md
@@ -0,0 +1,5 @@
+## This is Readme
+* vue.js 为 vue 官方开发版(即带测试和错误报告功能)文件
+* lodash4.16.0.js 为引入的 lodash 库(封装了对数组,对象等数据类型的处理函数,极好的浏览器兼容性和人性化的处理方式)
+* dynamics.js dynamics库为基于物理的动画工具库
+
