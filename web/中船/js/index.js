b3c58ba9c13cca2b6ea24fa422676893fc2aedc3
ove-wak <ove.wak@gmail.com>
2018-03-16 14:29:13 +0800

    0316

--- web/中船/js/index.js
+++ web/中船/js/index.js
@@ -1,51 +1,162 @@
+$(window).resize(function(){ 
+    document.getElementById("pie-doughnut-1").removeAttribute("_echarts_instance_");
+    document.getElementById("pie-doughnut-2").removeAttribute("_echarts_instance_"); 
+    pie1();
+    pie2();
+});
 $(document).ready(function(){ 
+    pie1();
+    pie2();
+});
+
+function pie1(){
     var echart = echarts.init(document.getElementById('pie-doughnut-1'));  
-    mylabel={
-                show:true,                  //是否显示标签。
-                position:"right",          //标签的位置。// 绝对的像素值[10, 10],// 相对的百分比['50%', '50%'].'top','left','right','bottom','inside','insideLeft','insideRight','insideTop','insideBottom','insideTopLeft','insideBottomLeft','insideTopRight','insideBottomRight'
-                offset:[30, 40],             //是否对文字进行偏移。默认不偏移。例如：[30, 40] 表示文字在横向上偏移 30，纵向上偏移 40。
-                formatter:'{b}',       //标签内容格式器。模板变量有 {a}、{b}、{c}，分别表示系列名，数据名，数据值。
-            };
     option = {
-            
-            series: [
+        animation: false,
+        tooltip: {
+            show:true,
+            trigger: 'item',
+            formatter: function(params, ticket, callback){
+                var num = '29';
+                var str = '<div style="text-align: center" id="toolTipBox"><p style="font-size:0.4rem;margin:0px">'+ num + '</p></div>'
+                return str
+            },
+            position: function (point, params, dom, rect, size) {
+                //size参数可以拿到提示框的outerWidth和outerheight，dom参数可以拿到提示框div节点。
+                //console.log(dom)可以看到，提示框是用position定位
+                var marginW = Math.ceil(size.contentSize[0]/2);
+                var marginH = Math.ceil(size.contentSize[1]/2);
+                dom.style.marginLeft='-' + marginW + 'px';
+                dom.style.marginTop='-' + marginH + 'px';
+                return ["44%", "52%"];
+            },
+            alwaysShowContent:true,
+            backgroundColor:false,  //设置提示框的背景色
+            textStyle:{
+                color:'#00d2ff'
+            }
+        },          
+        series: [{
+            name:'目标管控',
+            type:'pie',
+            radius: ['60%', '90%'],
+            center: ["44%", "52%"],
+            silent:true,
+            avoidLabelOverlap: false,
+            hoverAnimation:false,   //关闭 hover 在扇区上的放大动画效果。
+            cursor:'default', //鼠标悬浮时在图形元素上时鼠标的样式是什么。同 CSS 的 cursor。
+            itemStyle: {
+                normal : { //normal 是图形在默认状态下的样式；emphasis 是图形在高亮状态下的样式，比如在鼠标悬浮或者图例联动高亮时。
+                    label : {  //饼图图形上的文本标签
+                        show : false
+                    },
+                    labelLine : {     //标签的视觉引导线样式
+                        show : false
+                    }, 
+                },
+                emphasis: {  
+                    show : false
+                }  
+            } ,
+            data:[
                 {
-                    name:'目标管控',
-                    type:'pie',
-                    radius: ['50%', '70%'],
-                    avoidLabelOverlap: false,
-                    silent:true,
-                    itemStyle: {
-                    normal : { //normal 是图形在默认状态下的样式；emphasis 是图形在高亮状态下的样式，比如在鼠标悬浮或者图例联动高亮时。
-                            label : {  //饼图图形上的文本标签
-                                show : true, 
-                                formatter: "{d}" 
-                            },
-                            labelLine : {     //标签的视觉引导线样式
-                                show : true ,
-                                position:"right" 
-                            }
-                        }, 
-                        emphasis: {  
-                            show : false
-                        }  
-                    } ,
-                    data:[
-                        {
-                        value:5,  
-                        itemStyle:{
-                            normal:{color:'#fff100'}
-                        },
-                        name:'17.2%'},
-                        {value:24,
-                        itemStyle:{
-                            normal:{color:'#5ae06d'}
-                        }, 
-                        name:'82.8%'}
-                    ]
+                    value:5,  
+                    itemStyle:{
+                        normal:{color:'#fff100'}
+                    },
+                    name:'黄色'
+                },
+                {
+                    value:24,
+                    itemStyle:{
+                        normal:{color:'#5ae06d'}
+                    }, 
+                    name:'绿色'
                 }
             ]
+        }]
     };
                   
     echart.setOption(option);  
-});
+    echart.dispatchAction({
+        type: 'showTip',
+        seriesIndex: 0,
+        dataIndex: 0
+    });
+}
+
+function pie2(){
+    var echart = echarts.init(document.getElementById('pie-doughnut-2'));  
+    option = {
+        animation: false,
+        tooltip: {
+            show:true,
+            trigger: 'item',
+            formatter: function(params, ticket, callback){
+                var num = '26';
+                var str = '<div style="text-align: center" id="toolTipBox"><p style="font-size:0.4rem;margin:0px">'+ num + '</p></div>'
+                return str
+            },
+            position: function (point, params, dom, rect, size) {
+                //size参数可以拿到提示框的outerWidth和outerheight，dom参数可以拿到提示框div节点。
+                //console.log(dom)可以看到，提示框是用position定位
+                var marginW = Math.ceil(size.contentSize[0]/2);
+                var marginH = Math.ceil(size.contentSize[1]/2);
+                dom.style.marginLeft='-' + marginW + 'px';
+                dom.style.marginTop='-' + marginH + 'px';
+                return ["44%", "52%"];
+            },
+            alwaysShowContent:true,
+            backgroundColor:false,  //设置提示框的背景色
+            textStyle:{
+                color:'#00d2ff'
+            }
+        },          
+        series: [{
+            name:'目标管控',
+            type:'pie',
+            radius: ['60%', '90%'],
+            center: ["44%", "52%"],
+            silent:true,
+            avoidLabelOverlap: false,
+            hoverAnimation:false,   //关闭 hover 在扇区上的放大动画效果。
+            cursor:'default', //鼠标悬浮时在图形元素上时鼠标的样式是什么。同 CSS 的 cursor。
+            itemStyle: {
+                normal : { //normal 是图形在默认状态下的样式；emphasis 是图形在高亮状态下的样式，比如在鼠标悬浮或者图例联动高亮时。
+                    label : {  //饼图图形上的文本标签
+                        show : false
+                    },
+                    labelLine : {     //标签的视觉引导线样式
+                        show : false
+                    }, 
+                },
+                emphasis: {  
+                    show : false
+                }  
+            } ,
+            data:[
+                {
+                    value:6,  
+                    itemStyle:{
+                        normal:{color:'#fff100'}
+                    },
+                    name:'黄色'
+                },
+                {
+                    value:20,
+                    itemStyle:{
+                        normal:{color:'#5ae06d'}
+                    }, 
+                    name:'绿色'
+                }
+            ]
+        }]
+    };
+                  
+    echart.setOption(option);  
+    echart.dispatchAction({
+        type: 'showTip',
+        seriesIndex: 0,
+        dataIndex: 0
+    });
+}
