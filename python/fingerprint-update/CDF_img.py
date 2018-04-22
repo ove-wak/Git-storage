604564e7ea7be2ebe203ddf72837fd6bde9de81a
ove_wak <ove.wak@gmail.com>
2018-04-22 15:07:11 +0800

    0422

--- python/fingerprint-update/CDF_img.py
+++ python/fingerprint-update/CDF_img.py
@@ -14,11 +14,13 @@ with open("3_cdf.csv","r") as csvfile:
 for x in range(len(data)):
     data[x]= int(data[x])
 sorted_data = np.sort(data)
+print(sorted_data)
 yvals=np.arange(len(sorted_data))/float(len(sorted_data)-1)
+print(yvals)
 popt, pcov = curve_fit(func, sorted_data, yvals)  
 #popt数组中，三个值分别是待求参数a,b,c  
 y_t = [func(i, popt[0],popt[1],popt[2]) for i in sorted_data]  
-plt.plot(sorted_data,y_t,label="After 3 days",linestyle='--')
+plt.plot(sorted_data,yvals,label="After 3 days",linestyle='--')
 
 with open("6_cdf.csv","r") as csvfile:
      #读取csv文件，返回的是迭代类型
@@ -29,8 +31,8 @@ for x in range(len(data)):
     data[x]= int(data[x])
 sorted_data = np.sort(data)
 yvals=np.arange(len(sorted_data))/float(len(sorted_data)-1) 
-print(sorted_data)
-print(yvals)
+# print(sorted_data)
+# print(yvals)
 popt, pcov = curve_fit(func, sorted_data, yvals)  
 #popt数组中，三个值分别是待求参数a,b,c  
 y_t = [func(i, popt[0],popt[1],popt[2]) for i in sorted_data]  
@@ -49,6 +51,20 @@ popt, pcov = curve_fit(func, sorted_data, yvals)
 #popt数组中，三个值分别是待求参数a,b,c  
 y_t = [func(i, popt[0],popt[1],popt[2]) for i in sorted_data]  
 plt.plot(sorted_data,y_t,label="After 10 days")
+with open("0_cdf.csv","r") as csvfile:
+     #读取csv文件，返回的是迭代类型
+     read = csv.reader(csvfile)
+     for i in read:
+          data = i
+for x in range(len(data)):
+    data[x]= int(data[x])
+sorted_data = np.sort(data)
+yvals=np.arange(len(sorted_data))/float(len(sorted_data)-1)
+popt, pcov = curve_fit(func, sorted_data, yvals)  
+#popt数组中，三个值分别是待求参数a,b,c  
+y_t = [func(i, popt[0],popt[1],popt[2]) for i in sorted_data]  
+plt.plot(sorted_data,y_t,label="After 0 days")
+
 plt.xlabel("RSS prediction error[dB]",fontsize=20)
 plt.ylabel("CDF",fontsize=20)
 plt.axis([0,30,0,1])
--- python/fingerprint-update/make_CDF_data.py
+++ python/fingerprint-update/make_CDF_data.py
@@ -3,7 +3,7 @@ import xlrd,os,time,numpy,csv
 
 # 生成中间数据
 # def get_median(self):
-j = 3
+j = 10
 
 cdf_data = []
 for x in range(4):
@@ -15,7 +15,8 @@ for x in range(4):
     for i in range(1,nrows):
         data.append(table.row_values(i)[1:])
 
-    file = xlrd.open_workbook('excel/'+str(j)+'oselm.xls')
+    # file = xlrd.open_workbook('excel/'+str(j)+'oselm.xls')
+    file = xlrd.open_workbook('excel/0.xls')
     table = file.sheets()[sheet]  #通过索引顺序获取工作表
     nrows = table.nrows #行数
     data_t = []
@@ -29,6 +30,6 @@ for x in range(4):
                 temp = abs(int(data[y][x]-data_t[y][x]))
                 if temp <= 30:
                     cdf_data.append(temp)
-with open(str(j)+"_cdf.csv","w") as csvfile: 
+with open(str(0)+"_cdf.csv","w") as csvfile: 
     writer = csv.writer(csvfile)
     writer.writerow(cdf_data)
