e88c1971e9af095102e28bf63dad95c0500f5f46
ove_wak <ove.wak@gmail.com>
2018-04-22 14:05:02 +0800

    0422

--- python/fingerprint-update/CDF_img.py
+++ python/fingerprint-update/CDF_img.py
@@ -18,7 +18,7 @@ yvals=np.arange(len(sorted_data))/float(len(sorted_data)-1)
 popt, pcov = curve_fit(func, sorted_data, yvals)  
 #popt数组中，三个值分别是待求参数a,b,c  
 y_t = [func(i, popt[0],popt[1],popt[2]) for i in sorted_data]  
-plt.plot(sorted_data,y_t)
+plt.plot(sorted_data,y_t,label="After 3 days",linestyle='--')
 
 with open("6_cdf.csv","r") as csvfile:
      #读取csv文件，返回的是迭代类型
@@ -34,7 +34,7 @@ print(yvals)
 popt, pcov = curve_fit(func, sorted_data, yvals)  
 #popt数组中，三个值分别是待求参数a,b,c  
 y_t = [func(i, popt[0],popt[1],popt[2]) for i in sorted_data]  
-plt.plot(sorted_data,y_t)
+plt.plot(sorted_data,y_t,label="After 6 days",linestyle='-.')
 
 with open("10_cdf.csv","r") as csvfile:
      #读取csv文件，返回的是迭代类型
@@ -48,6 +48,10 @@ yvals=np.arange(len(sorted_data))/float(len(sorted_data)-1)
 popt, pcov = curve_fit(func, sorted_data, yvals)  
 #popt数组中，三个值分别是待求参数a,b,c  
 y_t = [func(i, popt[0],popt[1],popt[2]) for i in sorted_data]  
-plt.plot(sorted_data,y_t)
-
+plt.plot(sorted_data,y_t,label="After 10 days")
+plt.xlabel("RSS prediction error[dB]",fontsize=20)
+plt.ylabel("CDF",fontsize=20)
+plt.axis([0,30,0,1])
+plt.grid(linestyle='--')
+plt.legend(loc=4,fontsize=20)
 plt.show()
