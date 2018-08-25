aaa8742f839763185af756fa70480aeb6cd91c88
ove_wak <ove.wak@gmail.com>
2018-08-25 16:34:07 +0800

    express server

--- web/nodejs-express-webserver/app.js
+++ web/nodejs-express-webserver/app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var todoRouter = require('./routes/todo');
+
 
 var app = express();
 var router = express.Router();
@@ -19,9 +21,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/index', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/todo', todoRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
