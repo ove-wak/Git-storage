3ded75813787c0cb8e7dbae0521261bfaa5e46cc
ove-wak <ove.wak@gmail.com>
2018-08-30 17:17:27 +0800

    client

--- python/client.py
+++ python/client.py
@@ -5,36 +5,21 @@ class myThread (threading.Thread):
         threading.Thread.__init__(self)
         self.counter = counter
     def run(self):
-        print ("开始线程：")
+        print("start：", self.counter)
         connect_clint(self.counter)
-        print ("退出线程：")
+        print("stop：", self.counter)
 
 def connect_clint(x):
     postdata = json.dumps({'number':x})
     # print(postdata)
-    r = requests.Session().post("http://39.105.99.125:9601",data=postdata.encode("utf-8"), headers={'Content-Type': 'application/x-www-form-urlencoded'})
-    print(r.content)
-thread1 = myThread(1)
-# thread2 = myThread(2)
-# thread3 = myThread(3)
-# thread4 = myThread(4)
-# thread5 = myThread(5)
-# thread6 = myThread(6)
-# thread7 = myThread(7)
-thread1.start()
-# thread2.start()
-# thread3.start()
-# thread4.start()
-# thread5.start()
-# thread6.start()
-# thread7.start()
-# thread1.join()
-# thread2.join()
-# thread3.join()
-# thread4.join()
-# thread5.join()
-# thread6.join()
-# thread7.join()
+    r = requests.Session().get('http://203.207.224.107:8281/fingerprint/download?buildinginfo=[{"buildingid":"LIESMARS","floor":[]}]')
+    # print(r.content)
 
-#print(r)
-#print(r.text)
+tl = []
+
+for i in range(10000):
+    tl.append(myThreadi))
+
+for i in range(10000):
+    tl[i].start()
+
