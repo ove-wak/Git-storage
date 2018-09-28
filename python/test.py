be65dba47d54580c05879516d22a378f66adc3c2
ove-wak <ove.wak@gmail.com>
2018-09-28 23:13:11 +0800

    0928

--- python/test.py
+++ python/test.py
@@ -1,19 +1,14 @@
-# 斑马题
-str = input()
-result=[]
-temp=0
-for i in range(len(str)-1):
-    if str[i] != str[i+1]:
-        temp=temp+1
-    else:
-        result.append(temp+1)
-        temp=0
-result.append(temp+1)
-if str[0] != str[-1]:
-    result[0] = result[0]+result[-1]
-res=0
-for r in result:
-    if res<r:
-        res=r
 
-print(res)
+f_num = input()
+num_ar = []
+for x in range(int(f_num)):
+    temp = input()
+    num_ar.append(temp)
+for num in num_ar:
+    t = 0
+    for i in range(len(num)):
+        t = t + int(num[i])
+    if int(num) % t :
+        print("NO")
+    else:
+        print("YES")
